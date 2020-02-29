ba016177e545a370b9c1064d5165f43dc27e55dc
Lukas <lukashaizinger@gmail.com>
2020-02-29 13:17:48 +0100

    test

--- src/Program.cs
+++ src/Program.cs
@@ -21,7 +21,9 @@ namespace FibonacciSeries {
       int[] fibonacci = new int[count];
       fibonacci[0] = 0;
       fibonacci[1] = 1;
-
+	  
+	  
+	  
       for (int i = 2; i < count; i++) {
         fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
       }
